d6d1f13b89c84afcc4ff91c498cf4ea11c7e6323
ijlnflhbrzdev <ijlnflhbrzdev@gmail.com>
2023-02-16 10:24:14 +0700

    UPDATE GALERY AND CHANGE FILE INDEX

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// GALERY CAROUSEL OWL
+
+	// Gallery Slider JS
+	$('.gallery-slider-three').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='flaticon-left-arrow'></i>",
+			"<i class='flaticon-right-arrow'></i>",
+		],
+		responsive:{
+			0:{
+				items: 1,
+			},
+			576:{
+				items: 2,
+			},
+			768:{
+				items: 2,
+			},
+			992:{
+				items: 4,
+			},
+			1200:{
+				items: 4,
+			}
+		}
+	});
